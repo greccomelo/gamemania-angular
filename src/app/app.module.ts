2cbc842f248869c15804ac25bd0b3665c5b09e3f
grecco <greccomelo@gmail.com>
2022-01-11 17:34:33 -0300

    Atualizados 11/01/2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +27,7 @@ import { NotfoundComponent } from './views/notfound/notfound.component';
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
-    HttpHeaders
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
